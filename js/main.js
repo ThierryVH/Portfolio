daa35836cbdd5dabb514e7d340357419a9414f65
Thierry Van-hecke <thierry.vhecke@gmail.com>
2018-02-03 17:50:54 +0100

    test form

--- js/main.js
+++ js/main.js
@@ -1,10 +1,53 @@
 function openNav() {
   document.getElementsByClassName("mySidebar")[0].style.display = "block";
   document.getElementsByClassName("mySidebar")[1].style.display = "block";
-	document.getElementsByClassName('button')[0].style.display = "none";
+	document.getElementById('button').style.display = "none";
 }
 function closeNav() {
   document.getElementsByClassName("mySidebar")[0].style.display = "none";
   document.getElementsByClassName("mySidebar")[1].style.display = "none";
-	document.getElementsByClassName('button')[0].style.display = "block";
+	document.getElementById('button').style.display = "block";
+
 }
+
+
+// var typed = new Typed('#typed', {
+//   strings: ["First sentence.", "Second sentence."],
+//   typeSpeed: 100
+// });
+
+$(function(){
+	$("#typed").typed({
+		strings: ["Développeur Web.", "Formateur Développeur Web."],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 100,
+		// time before typing starts
+		startDelay: 700,
+		// backspacing speed
+		backSpeed: 50,
+		// time before backspacing
+		backDelay: 1000,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: false,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});
